8b692ff808f44174aed993e8cc2f424792466b32
Kelsangg <58080314+Kelsangtsomo@users.noreply.github.com>
2021-01-07 20:34:16 -0500

    hi

---
+++ src/like/AVillian.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package like;
+import hsa_new.Console;
+
+/**
+ * @author OWNER
+ *
+ */
+public class AVillian {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Console c = new Console();
+		c.println("hi");
+	}
+
+}
